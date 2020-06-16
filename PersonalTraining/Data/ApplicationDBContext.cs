2082cf91c869418586d91f617cbc243d76313c9e
Randall Gray <randall.gray7260@gmail.com>
2020-06-16 13:53:20 -0500

    Initial migration. ERD diagram complete.

--- PersonalTraining/Data/ApplicationDBContext.cs
+++ PersonalTraining/Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PersonalTraining.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,16 @@ namespace PersonalTraining.Data
     public class ApplicationDBContext : DbContext
     {
         // Member variables
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<BroadcastMessage> BroadcastMessages { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Conversation> Conversations { get; set; }
+        public DbSet<ExerciseClass> ExerciseClasses { get; set; }
+        public DbSet<FAQ> FAQs { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Trainer> Trainers { get; set; }
+        public DbSet<Video> Videos { get; set; }
 
         // Constructor
         public ApplicationDBContext(DbContextOptions options) : base(options)
